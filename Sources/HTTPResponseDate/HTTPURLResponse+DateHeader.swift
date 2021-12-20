247b5db2a904b192cb11aa7d5848592c277a4101
Jeremy Saklad <saklad5@me.com>
2021-12-19 18:34:03 -0600

    Cache DateFormatter instances
    Using NSCache allows DateFormatters to be reused without taking up valuable memory.
    
    HTTP date formats are now stored using a private enumeration.
    
    Access control is now specified on each declaration, rather than the extension.

--- Sources/HTTPResponseDate/HTTPURLResponse+DateHeader.swift
+++ Sources/HTTPResponseDate/HTTPURLResponse+DateHeader.swift
@@ -4,31 +4,35 @@ import Foundation
 import class FoundationNetworking.HTTPURLResponse
 #endif
 
-public extension HTTPURLResponse {
-	private static let dateFormatters = [
-		// GMT is required in all timestamps, but section 19.3 of RFC 2616 requires clients to convert timestamps incorrectly given with a different time zone into GMT.
+extension HTTPURLResponse {
+  private enum HTTPDate: NSString, CaseIterable {
+    /// RFC 1123
+    case rfc1123 = "EEE, dd MMM yyyy HH:mm:ss z"
 
-		// RFC 1123
-		"EEE, dd MMM yyyy HH:mm:ss z",
+    /// RFC 850
+    case rfc850 = "EEEE, dd-MMM-yy HH:mm:ss z"
 
-		// RFC 850
-		"EEEE, dd-MMM-yy HH:mm:ss z",
+    /// ANSI C's asctime() format
+    case asctime = "EEE MMM d HH:mm:ss yyyy"
 
-		// ANSI C's asctime() format
-		"EEE MMM d HH:mm:ss yyyy",
-		]
-		.lazy // It's very unlikely that any DateFormatter beyond the first will be necessary.
-		.map { formatString -> DateFormatter in
-			let formatter = DateFormatter()
-			formatter.locale = Locale(identifier: "en_US_POSIX")
-			formatter.timeZone = TimeZone(identifier: "GMT")
-			formatter.dateFormat = formatString
-			return formatter
-	}
+    private static let cache = NSCache<NSString, DateFormatter>()
+    var formatter: DateFormatter {
+      if let result = Self.cache.object(forKey: rawValue) {
+        return result
+      } else {
+        let result = DateFormatter()
+        result.locale = Locale(identifier: "en_US_POSIX")
+        result.timeZone = TimeZone(identifier: "GMT")
+        result.dateFormat = rawValue as String
+        Self.cache.setObject(result, forKey: rawValue)
+        return result
+      }
+    }
+  }
 
 	/// The date that the response was sent.
 	/// - Note: This is parsed from the `date` header of the response according to [RFC 2616](https://tools.ietf.org/html/rfc2616#section-3.3.1).
-	var date: Date? {
+	public var date: Date? {
 		// Servers are required to send a date header whenever possible. It isn't always possible.
 		let dateValue: String?
 
@@ -46,12 +50,6 @@ public extension HTTPURLResponse {
 			return nil
 		}
 
-		for dateFormatter in Self.dateFormatters {
-			if let date = dateFormatter.date(from: dateString) {
-				return date
-			}
-		}
-
-		return nil
+    return HTTPDate.allCases.lazy.compactMap { $0.formatter.date(from: dateString) }.first
 	}
 }
